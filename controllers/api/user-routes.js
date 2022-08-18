a66baa63e0c7e6bd7efc785be1211dc2123b8c19
ausbosspoland <austinjpoland311@gmail.com>
2022-08-18 18:22:53 -0500

    kinda created login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/register', async (req, res) => {
-    try {
-        const { username, email, password, confirmPassword } = req.body
-        if (password !== confirmPassword) {
-            return res.status(400).json({ message: 'Passwords do not match' })
-        }
-        User.create({ username, email, password }).then((newUserData)=>{
-            req.session.save(()=>{
-                req.session.user_id = newUserData.id
-                res.json(newUserData)
-            })
-            
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: 'Error occured' })
-    }
-})
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.post('/login', (req, res)=>{
     try {
